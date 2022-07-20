2092cbc74b6b58da720326b273fbafdff85064e1
ShaileshKumar007 <shailesh1429k@gmail.com>
2022-07-20 11:12:11 +0530

    Added styles and redux initial files

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, componse } from 'redux';
+import thunk from 'redux-thunk';
 
 import App from './App'
+
+const store = createStore()
 
 ReactDOM.render(<App/>, document.getElementById('root'));
