50563b3f11b2f3fe6965d5e1b492d9eb9347a2fa
ShaileshKumar007 <shailesh1429k@gmail.com>
2022-07-20 23:08:22 +0530

    integrated the redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, componse } from 'redux';
 import thunk from 'redux-thunk';
+import reducers from './reducers';
+import { configureStore } from '@reduxjs/toolkit'
 
 import App from './App'
 
-const store = createStore()
+const store = configureStore({ reducers })
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'));
---
+++ client/src/reducers/index.js
@@ -0,0 +1,5 @@
+import { combineReducers } from "redux";
+
+import posts from './posts'
+
+export default combineReducers({  posts });     // posts:posts but bcz key and value is same
