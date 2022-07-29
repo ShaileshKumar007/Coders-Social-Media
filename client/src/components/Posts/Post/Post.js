3cca2223fea16c8b8443458156ac43e7831f5615
ShaileshKumar007 <shailesh1429k@gmail.com>
2022-07-29 22:34:38 +0530

    completed the delete functionality

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -5,9 +5,12 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
 import moment from 'moment';     // used to represent sec,min, days ago in post eg. 15 minutes ago
 import useStyles from './styles';
+import { useDispatch } from 'react-redux';
+import { deletePost} from '../../../actions/posts';
 
 function Post({ post, setCurrentId }) {
   const classes = useStyles();
+  const dispatch = useDispatch(); 
   return (
     <Card className={classes.card}>
       <CardMedia className={classes.media} image={post.selectedFile} title={post.title} />
@@ -34,7 +37,7 @@ function Post({ post, setCurrentId }) {
           Like
           {post.likeCount}
         </Button>
-        <Button size='small' color='primary' onClick={() => {}}>
+        <Button size='small' color='primary' onClick={() => dispatch(deletePost(post._id))}>
           <DeleteIcon fontSize='small' />
           Delete
         </Button>
