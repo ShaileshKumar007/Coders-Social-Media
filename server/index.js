8a8aeee77e6574ffc4f021ce73a4d769e47f7563
ShaileshKumar007 <shailesh1429k@gmail.com>
2022-08-09 02:10:40 +0530

    done the auth for backend

--- server/index.js
+++ server/index.js
@@ -4,7 +4,8 @@ import mongoose from 'mongoose'
 import cors from 'cors'
 import dotenv from 'dotenv';
 
-import postroutes from './routes/posts.js'
+import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
 
 const app = express(); 
 dotenv.config();
@@ -13,7 +14,8 @@ app.use(bodyParser.json({limit:"30mb", extended:true}));
 app.use(bodyParser.urlencoded({limit:"30mb", extended:true}));
 app.use(cors());
 
-app.use('/posts', postroutes); 
+app.use('/posts', postRoutes); 
+app.use('/user', userRoutes); 
 
 const PORT = process.env.PORT || 5000;
 
